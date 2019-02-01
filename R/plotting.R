4d6d2b96980739b318e05e8ee7cc71ee0ac90b30
yutong <yutongw@umich.edu>
2019-02-01 10:19:29 -0500

    Liver bud vignette complete.

--- R/plotting.R
+++ R/plotting.R
@@ -17,7 +17,7 @@ get_shape_legend <- function(batch,my_shape_palette){
   n_cells <- length(batch)
   cowplot::get_legend(
     ggplot2::qplot(1:n_cells, 1:n_cells, shape = batch) +
-      my_shape_palette +
+      scale_shape_manual(values = my_shape_palette) +
       guides(shape=guide_legend(title="Batch")) +
       theme(legend.title.align=0.5)
   )
@@ -28,15 +28,16 @@ get_color_legend <- function(cell_type,my_color_palette,ncol = NULL,legend.posit
   n_cells <- length(cell_type)
   cowplot::get_legend(
     ggplot2::qplot(1:n_cells, 1:n_cells, color = cell_type) +
-      my_color_palette +
+      scale_color_manual(values = my_color_palette) +
       guides(col = guide_legend(title= legend.title, ncol = ncol)) +
       theme(legend.position = legend.position)
     )
 }
 
+#' Returns an empty plot with just the axes and the axes labels
 #' @export
 get_axes_legend <- function(emb_name){
-  qplot(iris[, 1], iris[, 2], asp = 1, shape = NA) +
+  qplot(iris[, 1], iris[, 2], asp = 1, shape = NA, na.rm = TRUE) +
     xlab(paste0(emb_name, 1)) +
     ylab(paste0(emb_name, 2)) +
     theme(
